13121999fc369bb9235490580d66f51204a02988
John O. Paul <oyomthojohnpaul@gmail.com>
2018-01-30 12:51:48 +0300

    feat(templating): create PageHeader component

---
+++ src/pages/myActivities/MyActivities.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PageHeader from '../../components/pageHeader/PageHeader.component';
+
+import './MyActivities.scss';
+
+export default () => (
+  <div className="myActivities">
+    <PageHeader title="My Activities" />
+  </div>
+);
