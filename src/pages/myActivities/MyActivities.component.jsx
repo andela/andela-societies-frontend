8fc84108ccab8fa1b53068e1b085f678746dc138
John O. Paul <oyomthojohnpaul@gmail.com>
2018-01-30 13:54:43 +0300

    feat(templating): add ActivityCard component

--- src/pages/myActivities/MyActivities.component.jsx
+++ src/pages/myActivities/MyActivities.component.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 import PageHeader from '../../components/pageHeader/PageHeader.component';
+import ActivityCard from '../../components/activityCard/ActivityCard.component';
 
 import './MyActivities.scss';
 
 export default () => (
   <div className="myActivities">
     <PageHeader title="My Activities" />
+    <div className="activities">
+      <ActivityCard
+        category="Participating in a tech event"
+        date="November 3, 2017"
+        description="Mentored teens how to code. (DBC 2016 at Redemption camp)"
+        points="250"
+        status="expired"
+      />
+    </div>
   </div>
 );
