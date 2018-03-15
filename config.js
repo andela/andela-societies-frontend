8bfeca4358a96ea1976febc26e4f1b8cff23c421
mkiterian <hmukite@gmail.com>
2018-03-15 10:44:51 +0300

    [Fix] Add production config

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ const configs = {
     APP_URL: 'http://soc-dev.andela.com:3000/',
     API_URL: '',
   },
+  production: {
+    ANDELA_API_BASE_URL: 'https://api.andela.com/login?redirect_url=',
+    APP_URL: 'https://andela-societies.herokuapp.com/',
+    API_URL: '',
+  },
   testing: {
     ANDELA_API_BASE_URL: 'https://api-staging.andela.com/login?redirect_url=',
     APP_URL: 'http://localhost:3000/',
