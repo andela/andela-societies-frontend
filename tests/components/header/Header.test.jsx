87c0f813093d93a2ecc7be580578e6f2d61c0b2a
Kingsley Solomon <kingsley.solomon@andela.com>
2018-03-26 14:46:28 +0100

    fix failing tests after merge

--- tests/components/header/Header.test.jsx
+++ tests/components/header/Header.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Header from '../../../src/components/header/Header';
+import { nonFellowTokenInfo } from '../../__mocks__/tokenInfoMock';
 
 describe('<Header />', () => {
   it('should render without crashing', () => {
-    expect(mount.bind(null, <Header />)).not.toThrow();
+    expect(mount.bind(null, <Header userInfo={nonFellowTokenInfo.UserInfo} />)).not.toThrow();
   });
 });
