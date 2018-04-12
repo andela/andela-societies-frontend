ce1e1e26923776442d69f0d7f3044dd38dd91399
farhanisto <farhanisto@gmail.com>
2018-04-12 13:05:45 +0300

    Added a HOC to show the Verify Activity page for the success department

--- src/components/header/SocietyButtons.jsx
+++ src/components/header/SocietyButtons.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import PageHeader from '../header/PageHeader';
 
-const SocietyButtons = () => (
-  <div className='societyButtons'>
-    <button className='societyButtons societyButtons__defaultSociety'>Istelle</button>
-    <button className='societyButtons societyButtons__invictus'>Invictus</button>
-    <button className='societyButtons societyButtons__sparks'>Sparks</button>
-    <button className='societyButtons societyButtons__phoniex'>Phoniex</button>
-    <div className='dropdown'>
-      <PageHeader />
-    </div>
-  </div>
+const Buttons = ({ filter, children }) => (
+  <button className={`societyButtons societyButtons__${filter}`} onClick={e => e.preventDefault}>
+    {children}
+  </button>
 );
+Buttons.propTypes = {
+  filter: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+/* eslint-disable */
+class SocietyButtons extends Component {
+ render() {
+   return (
+     <div className='societyButtons'>
+       <Buttons filter='defaultSociety'>istele</Buttons>
+       <Buttons filter='Invictus'>Invictus</Buttons>
+       <Buttons filter='Sparks'>Sparks</Buttons>
+       <Buttons filter='Phoniex'>Phoniex</Buttons>
+       <div className='dropdown'>
+       <PageHeader />
+       </div>
+     </div>
+   );
+ }
+}
 export default SocietyButtons;
