684443b0747d6a5b584091abc4a537b89db9bc6f
mkiterian <hmukite@gmail.com>
2018-06-05 17:54:36 +0300

    add changes requested in PR

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -1,11 +1,6 @@
 const validateFormFields = (formData) => {
-  const fieldArray = Object.keys(formData);
-  const errors = [];
-  for (let i = 0; i < fieldArray.length; i += 1) {
-    if (!formData[fieldArray[i]].trim().length) {
-      errors.push(fieldArray[i]);
-    }
-  }
+  let errors = [];
+  errors = Object.keys(formData).filter(fieldName => !formData[fieldName].trim().length);
   return errors;
 };
 
