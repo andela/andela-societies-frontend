c76d13e417cc0e602c03387b2afdf8d74423f40a
CrispusKamau <crispus.kamau@andela.com>
2018-07-13 01:15:38 +0300

    bug(https): change image link to  https (#91)
    - Change the URL of the image links to be fetched over https.
    - Point the API_BASE_URL to the staging backend.
    - Ensure deployments are only being made to the staging branch.
    
    [ Fixes #158916136 ]

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const configs = {
   staging: {
     AUTH_API: 'https://api.andela.com/login?redirect_url=',
     APP_URL: 'https://staging-societies.andela.com',
-    API_BASE_URL: 'https://private-ae5c2-andelasocietiesapi.apiary-mock.com/api/v1',
+    API_BASE_URL: 'https://api-staging-societies.andela.com/api/v1',
   },
   development: {
     AUTH_API: 'https://api.andela.com/login?redirect_url=',
