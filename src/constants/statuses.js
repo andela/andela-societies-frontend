be047e685b51a7bf5797e2f3bc5dcfa314fb76f6
puritybirir <purity.birir@andela.com>
2018-07-25 16:47:24 +0300

    Added completed status

--- src/constants/statuses.js
+++ src/constants/statuses.js
@@ -4,6 +4,7 @@ export const APPROVED = 'approved';
 export const PENDING = 'pending';
 export const REJECTED = 'rejected';
 export const IN_REVIEW = 'in review';
+export const COMPLETED = 'completed';
 
 export const STATUSES = {
   1: IN_REVIEW,
@@ -11,4 +12,5 @@ export const STATUSES = {
   3: APPROVED,
   4: REJECTED,
   5: ALL,
+  6: COMPLETED,
 };
