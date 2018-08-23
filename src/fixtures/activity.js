ec2b18a9ce2f3c4d53681823ba30452a9f5369d5
Birir Purity <purity.birir@andela.com>
2018-08-23 14:26:57 +0300

    Fix Eslint errors (#128)
    * Ignore unnecesary files
    
    * Fix all eslint errors

--- src/fixtures/activity.js
+++ src/fixtures/activity.js
@@ -2,6 +2,7 @@ const activity = {
   id: '8437fa68-8e6b-11e8-a05c-9801a7ae0330',
   category: 'Participating in a tech event',
   activityDate: 'November 3, 2017',
+  /* eslint max-len: ["error", { "ignoreStrings": true }] */
   description: 'Mentored teens how to code. (DBC 2016 at Redemption camp).Jim Shelton of ChanZuckerberginitiative sits down with Andela fellows',
   points: 250,
   status: 'approved',
