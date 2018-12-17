e89244fa09d01c776099e5ffb1c346d564a53a4a
Chris Maina <chris.maina@andela.com>
2018-12-17 10:54:21 +0300

    chore(setup): add HomeComponent

---
+++ src/Home/HomeComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default HomeComponent = () =>(
+    <h1>Welcome to Andela Societies </h1>
+);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HomeComponent from './Home'
+
+import './assets/scss/style.scss';
+
+ReactDOM.render(<HomeComponent />, document.getElementById('root'))
