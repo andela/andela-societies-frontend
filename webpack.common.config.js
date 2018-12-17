b1edcf5ad262e4bc21a2f68b2f495979bf87eadd
Chris Maina <chris.maina@andela.com>
2018-12-17 10:53:44 +0300

    chore(setup): add webpack configuration

---
+++ webpack.common.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].[chunkhash].bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'client', 'src'),
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // or test: /\.(png|woff|woff2|eot|ttf|svg)(\?.*$|$)$/
+        use: 'url-loader?limit=100000'
+      }
+    ]
+  }
+}
