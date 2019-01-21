9b5e3b6c4824f701bab61ef8705cb4a9eb4d0d5a
Chris Maina <chris.maina@andela.com>
2019-01-21 11:07:17 +0300

    feat(dashboard): add SidebarComponent

---
+++ src/app/common/components/SidebarComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * @name SidebarComponent
+ * @description used to navigate to different pages 
+ */
+export const SidebarComponent = (props) => {
+  return (
+    <aside>
+      
+    </aside>
+  );
+};
