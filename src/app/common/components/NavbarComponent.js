3003bd8507c06c39432673bdb1312a5e0a19d9a9
Chris Maina <chris.maina@andela.com>
2019-01-22 19:08:22 +0300

    feat(dashboard): add navbar component with its scss file

---
+++ src/app/common/components/NavbarComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const NavbarComponent = () => (
+  <nav className='navbar navbar-light'>
+    <form className='form-inline'>
+      <div className='input-group input-group-merge'>
+        <input
+          type='search'
+          aria-label='Search'
+          placeholder='Search ...'
+          className='form-control form-control-search'
+        />
+        <div className='input-group-append '>
+          <span className='input-group-text fa fa-search input-group-icon' id='basic-addon1' />
+        </div>
+      </div>
+    </form>
+  </nav>
+);
+
+export default NavbarComponent;
--- src/app/common/components/index.js
+++ src/app/common/components/index.js
@@ -1,3 +1,4 @@
 export { default as HeroComponent } from './HeroComponent';
 export { default as LogoComponent } from './LogoComponent';
+export { default as NavbarComponent } from './NavbarComponent';
 export { default as SidebarComponent } from './SidebarComponent';
