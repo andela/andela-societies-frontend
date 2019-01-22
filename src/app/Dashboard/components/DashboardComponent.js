d48b20037198bb3909f1eb95aa82c0fefad37175
Chris Maina <chris.maina@andela.com>
2019-01-22 21:57:03 +0300

    feat(dashboard): add Navbar component in dashboard
      - add css classes and styling in Navbar component

--- src/app/Dashboard/components/DashboardComponent.js
+++ src/app/Dashboard/components/DashboardComponent.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
 import { MyStatsComponent, SocietyStatsComponent } from './index';
-import { HeroComponent, SidebarComponent } from '../../common/components';
+import {
+  HeroComponent,
+  NavbarComponent,
+  SidebarComponent,
+} from '../../common/components';
 
 const DashboardComponent = () => (
   <div>
     <HeroComponent />
     <SidebarComponent />
     <div className='content-wrapper'>
-      <h2 className='profile__name'>Kevin Samoei</h2>
-      <h3 className='profile__level'>D2</h3>
-      <div>
-        <span />
-        <MyStatsComponent
-          points='1000'
-          activities='508'
-        />
-        <SocietyStatsComponent
-          usedPoints='1508'
-          remainingPoints='326'
-        />
+      <NavbarComponent />
+      <div className='user-dashboard'>
+        <h2 className='user-dashboard__name'>Kevin Samoei</h2>
+        <h3 className='user-dashboard__level'>D2</h3>
+        <div>
+          <span className='user-dashboard__image' />
+          <MyStatsComponent
+            points='1000'
+            activities='508'
+          />
+          <SocietyStatsComponent
+            usedPoints='1508'
+            remainingPoints='326'
+          />
+        </div>
       </div>
     </div>
   </div>
