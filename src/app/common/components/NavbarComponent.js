5a4ce5a3a1a93e2475d6bd14187d1d4815572e49
Chris Maina <chris.maina@andela.com>
2019-01-31 18:07:03 +0300

    feat(dashboard): add sidebar in navbar component
     - add media query for small and other devices

--- src/app/common/components/NavbarComponent.js
+++ src/app/common/components/NavbarComponent.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { LogoComponent, SidebarComponent } from './index';
 
-const NavbarComponent = () => (
-  <nav className='navbar navbar-light'>
-    <form className='form-inline col-sm-12'>
-      <div className='input-group input-group-merge'>
-        <input
-          type='search'
-          aria-label='Search'
-          placeholder='Search ...'
-          className='form-control form-control-search'
+class NavbarComponent extends Component {
+  state = {
+    sidebarState: false,
+  };
+
+  toggleSidebarState = () => {
+    this.setState(prevState => ({ sidebarState: !prevState.sidebarState }));
+  }
+
+  render() {
+    const { sidebarState } = this.state;
+    return (
+      <nav className='navbar navbar-light'>
+        <form className='form-inline'>
+          <div className='input-group input-group-merge'>
+            <input
+              type='search'
+              aria-label='Search'
+              placeholder='Search ...'
+              className='form-control form-control-search'
+            />
+            <div className='input-group-append '>
+              <span className='input-group-text fa fa-search input-group-icon' id='basic-addon1' />
+            </div>
+          </div>
+        </form>
+        <SidebarComponent
+          toggleSidebarState={this.toggleSidebarState}
+          className={sidebarState ? 'mobile-sidenav open' : 'mobile-sidenav close'}
         />
-        <div className='input-group-append '>
-          <span className='input-group-text fa fa-search input-group-icon' id='basic-addon1' />
+        <div className='navbar__actions'>
+          <i
+            tabIndex={0}
+            role='button'
+            data-toggle='collapse'
+            data-target='#menu-content'
+            onClick={this.toggleSidebarState}
+            onKeyPress={this.toggleSidebarState}
+            className='fa fa-bars fa-2x toggle-btn'
+          />
+          <LogoComponent
+            styles='navbar__logo'
+            logoClassType='logo__image--white'
+          />
         </div>
-      </div>
-    </form>
-  </nav>
-);
+      </nav>
+    );
+  }
+}
 
 export default NavbarComponent;
