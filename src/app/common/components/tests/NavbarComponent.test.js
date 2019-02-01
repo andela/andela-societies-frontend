751322806682b6c169160f3b6c1c0f7faabc35c2
Chris Maina <chris.maina@andela.com>
2019-02-01 19:10:10 +0300

    feat(dashboard): add css files in styles folder of each domain
     - add NavItemComponent tests
     - add Capitalize tests
     - add NavbarComponent tests
     - refactor image urls in logo.scss

--- src/app/common/components/tests/NavbarComponent.test.js
+++ src/app/common/components/tests/NavbarComponent.test.js
@@ -8,4 +8,13 @@ describe('<NavbarComponent />', () => {
   it('should have the class navbar', () => {
     expect(shallowWrapper.find('.navbar')).toHaveLength(1);
   });
+
+  it('should toggle sidebarState to true on invoking toggleSidebarState', () => {
+    const instance = shallowWrapper.instance();
+    instance.setState({
+      sidebarState: false
+    });
+    instance.toggleSidebarState();
+    expect(instance.state.sidebarState).toBe(true);
+  });
 });
