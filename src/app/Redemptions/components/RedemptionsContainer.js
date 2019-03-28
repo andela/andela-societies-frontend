8ff997a95b2d00b0a3a54f18c7e479e51361f2e0
Chris Maina <chris.maina@andela.com>
2019-03-28 12:47:27 +0300

    feat: add redemptions screen
     - add tests

---
+++ src/app/Redemptions/components/RedemptionsContainer.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import dashboardActions from '../../Dashboard/operations/actions';
+import societyActions from '../../Societies/operations/actions';
+import { getUserInfo, getToken } from '../../utils/tokenIsValid';
+
+import RedemptionsComponent from './RedemptionsComponent';
+import { SocietyStatsComponent } from '../../Dashboard/components';
+import { ButtonComponent, LoaderComponent } from '../../common/components';
+
+export class RedemptionsContainer extends Component {
+  static defaultProps = {
+    society: {},
+    societyName: '',
+    fetchUserActivites: null,
+    fetchSocietyRedemptionsRequest: null,
+  };
+
+  static propTypes = {
+    society: PropTypes.shape({}),
+    societyName: PropTypes.string,
+    fetchUserActivites: PropTypes.func,
+    fetchSocietyRedemptionsRequest: PropTypes.func,
+  };
+
+  componentDidMount() {
+    const { fetchSocietyRedemptionsRequest, societyName, fetchUserActivites } = this.props;
+    const token = getToken();
+    const userInfo = getUserInfo(token);
+    fetchUserActivites(userInfo.id);
+    fetchSocietyRedemptionsRequest(societyName.toLowerCase());
+  }
+
+  componentDidUpdate(prevProps) {
+    const { fetchSocietyRedemptionsRequest, societyName } = this.props;
+    if (prevProps.societyName !== societyName) {
+      fetchSocietyRedemptionsRequest(societyName.toLowerCase());
+    }
+  }
+
+  render() {
+    const { society, societyName } = this.props;
+    let verifyActivitiesHtml = (<LoaderComponent className='loader' />);
+    if (societyName) {
+      const {
+        usedPoints, pointsEarned, remainingPoints, activitiesLogged, redemptions,
+      } = society[
+        societyName.toLowerCase()
+      ];
+      // const inReviewActivities = this.filterActivitiesByInReviewStatus(loggedActivities);
+      verifyActivitiesHtml = (
+        <div>
+          <div className='profile-overview profile-overview--society'>
+            <div className={`profile-overview__image--society ${societyName.toLowerCase()}`} />
+            <SocietyStatsComponent
+              usedPoints={usedPoints}
+              totalPoints={pointsEarned}
+              remainingPoints={remainingPoints}
+              activitiesLogged={activitiesLogged}
+              className='society-page__stats'
+            />
+          </div>
+          <div className='user-dashboard__actions user-dashboard__actions--society col-sm-12'>
+            <h3 className='user-dashboard__title'>Latest Activities</h3>
+            <div>
+              <ButtonComponent className='button__add'>
+                <span className='fa fa-plus' />
+                <span>Log Points</span>
+              </ButtonComponent>
+              <ButtonComponent className='button__add button__redemption'>
+                <span className='fa fa-plus' />
+                <span>New Redemption</span>
+              </ButtonComponent>
+              <ButtonComponent className='button__filter'>
+                <span>Filter</span>
+                <span className='fa fa-filter' />
+              </ButtonComponent>
+            </div>
+          </div>
+          <RedemptionsComponent activities={redemptions} />
+        </div>
+      );
+    }
+    return verifyActivitiesHtml;
+  }
+}
+
+const mapStateToProps = ({ society, dashboard }) => ({
+  society,
+  societyName: dashboard.society,
+});
+
+const mapDispatchToProps = {
+  fetchSocietyRedemptionsRequest: societyActions.fetchSocietyRedemptionsRequest,
+  fetchUserActivites: dashboardActions.fetchUserActivitiesRequest,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(RedemptionsContainer);
