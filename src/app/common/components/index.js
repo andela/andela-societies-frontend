1918b6a087edbf15d180306d19db7eb23e487875
kachulio1 <jngugi88@gmail.com>
2019-04-05 23:23:22 +0300

    export filter component

--- src/app/common/components/index.js
+++ src/app/common/components/index.js
@@ -6,7 +6,12 @@ export { default as ButtonComponent } from './ButtonComponent';
 export { default as LoaderComponent } from './LoaderComponent';
 export { default as NavItemComponent } from './NavItemComponent';
 export { default as ProgressBarComponent } from './ProgressBarComponent';
-export { default as StatusIndicatorComponent } from './StatusIndicatorComponent';
+export {
+  default as StatusIndicatorComponent,
+} from './StatusIndicatorComponent';
 export { default as MenuItem } from './MenuItem';
-export { default as TruncateDescriptionContainer } from './TruncateDescriptionContainer';
+export {
+  default as TruncateDescriptionContainer,
+} from './TruncateDescriptionContainer';
 export { default as ToastMessageComponent } from './ToastMessageComponent';
+export { default as Filter } from './Filter';
