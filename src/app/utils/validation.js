407000cf3c5284d12c2a2566bb8c975f1a889dbe
alexmochu <mochualex4@gmail.com>
2019-04-11 17:35:22 +0300

    test

--- src/app/utils/validation.js
+++ src/app/utils/validation.js
@@ -8,12 +8,9 @@
 import validator from 'validator';
 
 const validatePointsModal = (data) => {
-  const numberofparticipatnts = validator.toInt(data.numberOfParticipants);
   const errors = {};
   (validator.isEmpty(data.categoryOption)) ? errors.categoryOption = 'Please select a category' : null;
   (validator.isEmpty(data.description)) ? errors.description = 'Please provide some a description' : null;
-  (data && data.selectCategory.supportsMultipleParticipants
-      && (numberofparticipatnts <= 0)) ? errors.numberOfParticipants = 'Should be atleast one participant' : null;
   return errors;
 };
 
