597d29c3d2cc1c6c36783cba81aff9a5bd2d1fbe
ajimae <meeky.ae@gmail.com>
2019-05-27 20:32:44 +0100

    feat(filter): filter society activities
    - import Filter component
    - add filter dropdown
    - add filter functionality
    
    [Finishes ######]

--- src/app/Societies/components/tests/SocietiesContainer.test.js
+++ src/app/Societies/components/tests/SocietiesContainer.test.js
@@ -26,13 +26,21 @@ describe('<SocietiesContainer />', () => {
         totalPoints: 200,
         activitiesLogged: activities.length,
         loggedActivities: activities,
-        redemptions: []
-      }
+        redemptions: [],
+      },
     },
     fetchSocietyInfoRequest: jest.fn(),
     fetchSocietyRedemptionsRequest: jest.fn()
   };
-  const shallowWrapper = shallow(<SocietiesContainer {...props} />);
+
+  let shallowWrapper;
+  beforeEach(() => {
+    shallowWrapper = shallow(<SocietiesContainer {...props} />);
+  });
+
+  afterEach(() => {
+    shallowWrapper.unmount();
+  });
 
   it('has a 2 ButtonComponents', () => {
     expect(shallowWrapper.find('ButtonComponent')).toHaveLength(2);
@@ -71,5 +79,275 @@ describe('<SocietiesContainer />', () => {
     });
     instance.handlePageClick(data);
     expect(instance.state.currentPage).toBe(1);
+  });
+
+  it('should show the filter drop down', () => {
+    const instance = shallowWrapper.instance();
+
+    instance.showFilter();
+    expect(instance.state.showFilterDropdown).toEqual(true);
+
+    shallowWrapper.unmount();
+  });
+
+  it('should hide the filter drop down', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        className: 'button__filter',
+      },
+    };
+
+    instance.hideFilter(event);
+    expect(instance.state.showFilterDropdown).toEqual(false);
+  });
+
+  it('should hide the filter drop down', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        current: '<span>Filter</span>',
+      },
+    };
+
+    instance.hideFilter(event);
+    expect(instance.state.showFilterDropdown).toEqual(false);
+  });
+
+  it('should select all when the first filter index is true', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select all',
+        checked: true,
+      },
+    };
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: false },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+    });
+
+    instance.handleClick(0)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(true);
+    expect(instance.state.filterBy[1].checked).toEqual(true);
+    expect(instance.state.filterBy[2].checked).toEqual(true);
+    expect(instance.state.filterBy[3].checked).toEqual(true);
+    expect(instance.state.filterBy[4].checked).toEqual(true);
+  });
+
+  it('should select a single checkbox', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select one',
+        checked: true,
+      },
+    };
+
+    const clcikNum = 0;
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: false },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+      clcikNum,
+    });
+
+    instance.handleClick(1)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(false);
+    expect(instance.state.filterBy[1].checked).toEqual(true);
+    expect(instance.state.filterBy[2].checked).toEqual(false);
+    expect(instance.state.filterBy[3].checked).toEqual(false);
+    expect(instance.state.filterBy[4].checked).toEqual(false);
+  });
+
+  it('should select another checkbox', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select one',
+        checked: true,
+      },
+    };
+
+    const clcikNum = 1;
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+      clcikNum,
+    });
+
+    instance.handleClick(2)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(false);
+    expect(instance.state.filterBy[1].checked).toEqual(true);
+    expect(instance.state.filterBy[2].checked).toEqual(true);
+    expect(instance.state.filterBy[3].checked).toEqual(false);
+    expect(instance.state.filterBy[4].checked).toEqual(false);
+  });
+
+  it('should uncheck one checkbox', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select one',
+        checked: false,
+      },
+    };
+
+    const clcikNum = 2;
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: true },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+      clcikNum,
+    });
+
+    instance.handleClick(2)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(false);
+    expect(instance.state.filterBy[1].checked).toEqual(true);
+    expect(instance.state.filterBy[2].checked).toEqual(false);
+    expect(instance.state.filterBy[3].checked).toEqual(false);
+    expect(instance.state.filterBy[4].checked).toEqual(false);
+  });
+
+  it('should uncheck another checkbox', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select one',
+        checked: false,
+      },
+    };
+
+    const clcikNum = 1;
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+      clcikNum,
+    });
+
+    instance.handleClick(1)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(false);
+    expect(instance.state.filterBy[1].checked).toEqual(false);
+    expect(instance.state.filterBy[2].checked).toEqual(false);
+    expect(instance.state.filterBy[3].checked).toEqual(false);
+    expect(instance.state.filterBy[4].checked).toEqual(false);
+
+    expect(instance.state.filteredSocietyActivities.length).toEqual(4);
+  });
+
+  it('should uncheck the \'select all\' checkbox when any of the checkbox is unchecked', () => {
+    const instance = shallowWrapper.instance();
+    const event = {
+      target: {
+        value: 'select all',
+        checked: true,
+      },
+    };
+
+    const filterBy = [
+      { name: 'select all', checked: false },
+      { name: 'approved', checked: false },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: false },
+    ];
+
+    instance.setState({
+      filterBy,
+    });
+
+    instance.handleClick(0)(event);
+    expect(instance.state.filterBy[0].checked).toEqual(true);
+    expect(instance.state.filterBy[1].checked).toEqual(true);
+    expect(instance.state.filterBy[2].checked).toEqual(true);
+    expect(instance.state.filterBy[3].checked).toEqual(true);
+    expect(instance.state.filterBy[4].checked).toEqual(true);
+
+    const eventTest = {
+      target: {
+        value: 'select all',
+        checked: false,
+      },
+    };
+    const filterByTest = [
+      { name: 'select all', checked: true },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: true },
+      { name: 'rejected', checked: false },
+      { name: 'pending', checked: true },
+    ];
+
+    instance.setState({
+      filterBy: filterByTest,
+    });
+
+    instance.handleClick(0)(eventTest);
+    expect(instance.state.filterBy[0].checked).toEqual(false);
+  });
+
+  it('should return a true if all the checkboxes are all checked', () => {
+    const instance = shallowWrapper.instance();
+    const item = [
+      { name: 'select all', checked: true },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: true },
+      { name: 'rejected', checked: true },
+      { name: 'pending', checked: true },
+    ];
+
+    const check = instance.isAllChecked(item);
+    expect(check).toEqual(true);
+  });
+
+  it('should return a false if at least one the checkboxes is unchecked', () => {
+    const instance = shallowWrapper.instance();
+    const item = [
+      { name: 'select all', checked: true },
+      { name: 'approved', checked: true },
+      { name: 'in review', checked: false },
+      { name: 'rejected', checked: true },
+      { name: 'pending', checked: true },
+    ];
+
+    const check = instance.isAllChecked(item);
+    expect(check).toEqual(false);
   });
 });
